cc9607c7c7d34a000d3c289fc2fae1fba2e9414e
Steve Ives <steve.ives@synergex.com>
2025-03-13 10:02:51 -0700

    Improve password decryption logic in the event that we encounter an unencryped or blank password.

--- PuTTYLauncher/DPAPIEncryption.cs
+++ PuTTYLauncher/DPAPIEncryption.cs
@@ -18,9 +18,20 @@ namespace PuTTYLauncher
 
         public static string Decrypt(string cipherText)
         {
-            byte[] data = Convert.FromBase64String(cipherText);
-            byte[] decrypted = ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
-            return Encoding.UTF8.GetString(decrypted);
+            try
+            {
+                byte[] data = Convert.FromBase64String(cipherText);
+                byte[] decrypted = ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
+                return Encoding.UTF8.GetString(decrypted);
+            }
+            catch (Exception)
+            {
+                //If we fail to decrypt, chanced are we either didn't have an encrypted password,
+                //or the configuration file came from some other user account or system.
+                //If we have an input string, return the same string, otherwise return blank.
+                return String.IsNullOrWhiteSpace(cipherText) ? String.Empty : cipherText;
+            }
+
         }
     }
 }
